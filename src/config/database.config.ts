c1655752b05aa6595293065487c18e625f15000f
Yeansovanvathana <61142466+Yeansovanvathana@users.noreply.github.com>
2023-08-10 10:16:26 +0700

    Add env

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,16 +1,16 @@
-import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const databaseConfig: PostgresConnectionOptions = {
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'bootcamp',
-    password: 'bootcamp',
-    database: 'flirtfever',
-    entities: [
-        __dirname + '/../**/*.entity{.ts,.js}',
-    ],
-    synchronize: true
-}
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: true,
+};
 
 export default databaseConfig;
